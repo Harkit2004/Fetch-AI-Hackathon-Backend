fdfa8cab8a39f501a072f6b89936f03605b84997
AlishaHaleem <halisha071@gmail.com>
2025-03-05 22:29:08 +0500

    Updated authentication and expense categorization logic

--- main.py
+++ main.py
@@ -7,18 +7,13 @@ from auth import hash_password, verify_password, create_access_token, get_curren
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.sql import func
 from expense_categorizer import categorize_expense  # Import categorization function
-from dotenv import load_dotenv
-import os
 from pydantic import BaseModel
 
 #  Initialize FastAPI app (only once)
 app = FastAPI()
 
 #  Load environment variables
-load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
 
 #  Ensure database tables are created
 Base.metadata.create_all(bind=engine)
